bce26c4aa1a58c4f55223fe0ce49810b501000d8
Andrewkha <chernyshov.andrey@gmail.com>
2020-08-04 09:48:30 +0300

    Chapt 1, ex 2

--- ch01-numbers/e02_summing_numbers.py
+++ ch01-numbers/e02_summing_numbers.py
@@ -1,13 +1,43 @@
-#!/usr/bin/env python3
-"""Solution to chapter 1, exercise 2: mysum"""
+def mysum(*args):
+    sum = 0
+    for i in args:
+        sum += i
+    return sum
 
 
-def mysum(*numbers):
-    """Accepts any number of numeric arguments as inputs.
-Returns the sum of those numbers.
-If invoked without any arguments, returns 0.
-"""
-    output = 0
-    for number in numbers:
-        output += number
-    return output
+def mysum_two_args(lst, start=0):
+    sum = start
+    for i in lst:
+        sum += i
+    return sum
+
+
+def average(args):
+    sum = mysum_two_args(args)
+    return sum / len(args)
+
+
+def strings_length(strings):
+    lengths = []
+    for word in strings:
+        lengths.append(len(word))
+    return max(lengths), min(lengths), average(lengths)
+
+
+def objects(lst):
+    filtered = []
+
+    for item in lst:
+        try:
+            filtered.append(int(item))
+        except Exception:
+            continue
+
+    return mysum_two_args(filtered)
+
+print(mysum(3, 5, 6, 7, 79))
+print(mysum_two_args([1, 2, 3], 4))
+print(average([1, 2, 3]))
+print(strings_length(['hello', 'd', 'thisisfuckingstring']))
+print(objects([1, 2.5, '1', 'sdsdsdsd', '10', [1, 2, 3], {'a': 1, 'b': 2}]))
+
