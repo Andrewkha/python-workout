525b72b13b7d756df593d4b3fdbfa8d0a2596e72
Andrewkha <chernyshov.andrey@gmail.com>
2020-08-15 12:13:45 +0300

    Chapt 2, ex 12 task1

--- ch03-lists-tuples/e12_most_repeating_letters.py
+++ ch03-lists-tuples/e12_most_repeating_letters.py
@@ -9,17 +9,21 @@ WORDS = ['this', 'is', 'an', 'elementary', 'test', 'example']
 
 def most_repeating_letter_count(word):
     """Given a non-empty string, counts how
-many times each letter appears in the string,
-and returns an integer indicating how often
-the most common letter appears."""
-
+    many times each letter appears in the string,
+    and returns an integer indicating how often
+    the most common letter appears."""
+    return operator.getitem(operator.getitem(Counter(word).most_common(), 0), 1)
 
 
 def most_repeating_word(words):
     """Given a list of non-empty strings (words),
-returns the word containing at least one letter that repeats
-more often than any letter in any other word.
+    returns the word containing at least one letter that repeats
+    more often than any letter in any other word.
 
-Because sorting in Python is stable, if multiple words have
-the same count, then the first will be returned."""
+    Because sorting in Python is stable, if multiple words have
+    the same count, then the first will be returned."""
 
+    return max(words, key=most_repeating_letter_count)
+
+
+print(most_repeating_word(WORDS))
