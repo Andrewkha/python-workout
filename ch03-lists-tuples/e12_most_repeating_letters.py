2930e2fc64b548f2fd2c13fed732c72798a5ed08
Andrewkha <chernyshov.andrey@gmail.com>
2020-08-17 14:41:41 +0300

    Chapt 2, ex 12 all

--- ch03-lists-tuples/e12_most_repeating_letters.py
+++ ch03-lists-tuples/e12_most_repeating_letters.py
@@ -5,6 +5,7 @@ from collections import Counter
 import operator
 
 WORDS = ['this', 'is', 'an', 'elementary', 'test', 'example']
+WORDS1 = ['this', 'is', 'aeiuiuuuuuuun', 'elementary', 'test', 'example']
 
 
 def most_repeating_letter_count(word):
@@ -26,4 +27,26 @@ def most_repeating_word(words):
     return max(words, key=most_repeating_letter_count)
 
 
+def count_vowels(word):
+    """
+    Instead of finding the word with the greatest number of repeated letters, find
+    the word with the greatest number of repeated vowels
+    """
+    vowels = 'aeuio'
+    filter(lambda x: x in vowels, word)
+    return most_repeating_letter_count(word)
+
+
+def most_repeating_vowel(words):
+    """Given a list of non-empty strings (words),
+    returns the word containing at least one letter that repeats
+    more often than any letter in any other word.
+
+    Because sorting in Python is stable, if multiple words have
+    the same count, then the first will be returned."""
+
+    return max(words, key=count_vowels)
+
+
 print(most_repeating_word(WORDS))
+print(most_repeating_word(WORDS1))
