f38f0540098aba4c435937f62c78b24d6f1df102
Andrewkha <chernyshov.andrey@gmail.com>
2020-09-11 14:19:14 +0300

    Completed

--- drunkman.py
+++ drunkman.py
@@ -4,7 +4,7 @@ Python emulation of the famous card game drunkman
 
 from random import shuffle
 from collections import deque
-import logging
+import csv
 
 deck = ['6', '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '8',
         '9', '9', '9', '9', '10', '10', '10', '10', 'J', 'J', 'J', 'J',
@@ -29,10 +29,16 @@ def beats(card1, card2):
     return False
 
 
-def main():
+def main(game):
+    """
+    implements main logic, returns number of steps taken
+    filename - the file to record the game progress 
+    """
     shuffle(deck)
     player1 = deque(deck[:18])
     player2 = deque(deck[18:])
+
+    log = []
 
     step = 0
 
@@ -42,57 +48,80 @@ def main():
         cards2 = [player2.popleft()]
 
         while cards1[-1] == cards2[-1]:
-            logging.info(f'Spor on {cards1[-1]} and {cards2[-1]}')
+            log.append(f'Spor on {cards1[-1]} and {cards2[-1]}')
             try:
                 hidden1 = player1.popleft()
             except IndexError:
-                logging.info('Player 1 out of cards')
+                log.append('Player 1 out of cards')
                 break
             cards1.append(hidden1)
-            logging.info(f'Player 1 has put {hidden1} as hidden card')
+            log.append(f'Player 1 has put {hidden1} as hidden card')
             try:
                 hidden2 = player2.popleft()
             except IndexError:
-                logging.info('Player 1 out of cards')
+                log.append('Player 2 out of cards')
                 break
             cards2.append(hidden2)
-            logging.info(f'Player 2 has put {hidden2} as hidden card')
+            log.append(f'Player 2 has put {hidden2} as hidden card')
 
             try:
                 open1 = player1.popleft()
-                cards1.append(open1)
-                logging.info(f'Player 1 has put {open1} as open card')
                 open2 = player2.popleft()
-                cards2.append(open2)
-                logging.info(f'Player 2 has put {open2} as open card')
             except IndexError:
-                logging.info(f'One of the players is out of the cards, using hidden carsds for the argue')
-                print('Using hidden cards for argue')
+                log.append(f'Using hidden cards for argue')
+                print(f'Game {game}: Using hidden cards for argue')
+            else:
+                cards1.append(open1)
+                log.append(f'Player 1 has put {open1} as open card')
+                cards2.append(open2)
+                log.append(f'Player 2 has put {open2} as open card')
 
         if beats(cards1[-1], cards2[-1]):
             player1.extend(cards1)
             player1.extend(cards2)
-            logging.info(f'One the step {step} Player 1 got {cards2[-1]} with {cards1[-1]}, '
+            log.append(f'One the step {step} Player 1 got {cards2[-1]} with {cards1[-1]}, '
                          f'the bank taken {cards1, cards2}')
-            logging.info(f'Players 1 cards: {player1}')
-            logging.info(f'Players 2 cards: {player2}')
+            log.append(f'Players 1 cards: {player1}')
+            log.append(f'Players 2 cards: {player2}')
         else:
             player2.extend(cards1)
             player2.extend(cards2)
-            logging.info(f'On the step {step} Player 2 got {cards1[-1]} with {cards2[-1]}, '
+            log.append(f'On the step {step} Player 2 got {cards1[-1]} with {cards2[-1]}, '
                          f'the bank taken {cards1, cards2}')
-            logging.info(f'Players 1 cards: {player1}')
-            logging.info(f'Players 2 cards: {player2}')
+            log.append(f'Players 1 cards: {player1}')
+            log.append(f'Players 2 cards: {player2}')
+
+        if step > 10000:
+            print(f'Game {game}: Looped')
+            log.append(f'Game {game}: Looped')
+            with open(f'drunk/game_{game}.txt', 'w') as file:
+                file.write('\n'.join(log))
+            return step
 
     if player1:
-        print(f'Player 1 wins! Number of steps {step}')
+        print(f'Game {game}: Player 1 wins! Number of steps {step}')
     else:
-        print(f'Player 2 wins! Number of steps {step}')
+        print(f'Game {game}: Player 2 wins! Number of steps {step}')
+
+    with open(f'drunk/game_{game}.txt', 'w') as file:
+        file.write('\n'.join(log))
 
     return step
 
 
 if __name__ == '__main__':
-        filename = f'drunk/drunkman_{1}.log'
-        logging.basicConfig(filename=filename, level=logging.INFO)
-        main()
+    steps = []
+    for i in range(1000000):
+        steps.append(main(i))
+
+    count_cycled = len([x for x in steps if x == 10001])
+    not_cycled = [x for x in steps if x != 10001]
+    steps = [(str(x),) for x in steps]
+    print(steps)
+    print(f"number of looped  games {count_cycled}")
+    print(f"avarage length of the game {sum(not_cycled) / len(not_cycled)}")
+    print(f"minimal steps count: {min(not_cycled)}")
+    print(f"maximum steps count: {max(not_cycled)}")
+    with open('drunker.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(steps)
