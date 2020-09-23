b6e4a3a236d7ff4f25cb03e53b45a7aef9be8233
Andrewkha <chernyshov.andrey@gmail.com>
2020-09-23 16:43:01 +0300

    ex 38 done

--- ch09-objects/e38_scoop.py
+++ ch09-objects/e38_scoop.py
@@ -5,13 +5,38 @@
 
 class Scoop():
     """Class representing a single scoop of ice cream.
-The sole attribute is "flavor", a string.
-"""
-
+    The sole attribute is "flavor", a string.
+    """
+    def __init__(self, flavour):
+        self.flavour = flavour
 
 
 def create_scoops():
     """Function that creates three scoops, puts them
-in a list, and iterates over that list, printing the
-flavors.
+    in a list, and iterates over that list, printing the
+    flavors.
+    """
+    scoops = [Scoop(x) for x in ('chocolate', 'vanilla', 'persimmon')]
+
+    for one in scoops:
+        print(one.flavour)
+
+
+create_scoops()
+
+
 """
+Create a new LogFile class that expects to be initialized with a filename.
+Inside of __init__, open the file for writing and assign it to an attribute,
+file, that sits on the instance. Check that it’s possible to write to the file via
+the file attribute.
+"""
+
+class LogFile:
+    def __init__(self, filename):
+        self.file = open(filename, 'w')
+
+
+logfile = LogFile('log.txt')
+logfile.file.write('rrrrr')
+logfile.file.close()
