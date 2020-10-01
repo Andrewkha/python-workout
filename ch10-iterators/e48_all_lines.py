51e844d01347526d28190f3479c4dbb656e797d2
Andrewkha <chernyshov.andrey@gmail.com>
2020-10-01 21:00:37 +0300

    ex 48 done

--- ch10-iterators/e48_all_lines.py
+++ ch10-iterators/e48_all_lines.py
@@ -3,13 +3,64 @@
 """Solution to chapter 10, exercise 48: all_lines"""
 
 
-import os
+from pathlib import Path
 
 
 def all_lines(path):
     """An iterator that returns, one at a time, each line
-from each file in a named directory.
+    from each file in a named directory.
+    Any file that cannot be opened, for whatever reason, is ignored.
+    """
+    pth = Path(path)
 
-Any file that cannot be opened, for whatever reason, is ignored.
-"""
+    files = (file for file in pth.iterdir() if file.is_file())
 
+    for file in files:
+        for line in open(file, encoding='utf-8'):
+            yield line
+
+
+def all_lines_tuple(path):
+    """
+    Modify all_lines such that it doesn’t return a string with each iteration, but
+    rather a tuple. The tuple should contain four elements: the name of the file,
+    the current number of the file (from all those returned by os.listdir), the
+    line number within the current file,
+    """
+    pth = Path(path)
+
+    files = (file for file in pth.iterdir() if file.is_file())
+
+    for index, file in enumerate(files):
+        for line_num, line in enumerate(open(file, encoding='utf-8')):
+            yield file.name, index + 1, line_num + 1, line
+
+
+def all_lines_diff_order(path):
+    """
+    The current version of all_lines returns all of the lines from the first file, then
+    all of the lines from the second file, and so forth. Modify the function such that it
+    returns the first line from each file, and then the second line from each file, until
+    all lines from all files are returned. When you finish printing lines from shorter
+    files, ignore those files while continuing to display lines from the longer files
+    """
+    pth = Path(path)
+
+    files = [file for file in pth.iterdir() if file.is_file()]
+    res = [[] for _ in files]
+    for file_index, file in enumerate(files):
+        for line in open(file):
+            res[file_index].append(line)
+
+    max_len = max(len(arr) for arr in res)
+
+    for line in range(0, max_len):
+        for arr in res:
+            try:
+                yield arr[line].strip()
+            except IndexError:
+                pass
+
+
+for one in all_lines_diff_order('../ch08-modules'):
+    print(one)
