b6a4555e298d2037e4b04fbef1b2607bc51b21fc
Andrewkha <chernyshov.andrey@gmail.com>
2020-10-01 21:42:42 +0300

    ex 49 done

--- ch10-iterators/e49_elapsed_since.py
+++ ch10-iterators/e49_elapsed_since.py
@@ -7,8 +7,46 @@ import time
 
 def elapsed_since(data):
     """A generator that takes an iterable as input.
+    With each iteration, it yields a tuple containing the
+    data and the time since the previous iteration.
+    """
 
-With each iteration, it yields a tuple containing the
-data and the time since the previous iteration.
-"""
+    last_time = None
 
+    for item in data:
+        curr_time = time.perf_counter()
+        delta = curr_time - (last_time or curr_time)
+        last_time = time.perf_counter()
+        yield item, delta
+
+
+def iter_func(iterable, func):
+    """Write a generator function that takes two elements: an iterable and a function.
+    With each iteration, the function is invoked on the current element. If the
+    result is True, then the element is returned as is. Otherwise, the next element is
+    tested, until the function returns True. Alternative: implement this as a regular
+    function that returns a generator expression"""
+
+    for element in iterable:
+        if func(element):
+            yield element
+
+
+def iter_func_expr(iterable, func):
+    """
+    Alternative: implement this as a regular
+    function that returns a generator expression
+    """
+
+    return (element for element in iterable if func(element))
+
+#
+# for one in elapsed_since('jrtyip'):
+#     print(one)
+
+
+for one in iter_func_expr('AsWedERT', str.isupper):
+    print(one)
+
+for one in iter_func('AsWedERT', str.isupper):
+    print(one)
